5ea8cc8ef289e3690070ee94d326a55485796c4b
tamyachilcanan-glitch <tamya.chilcanan@yachaytech.edu.ec>
2026-09-15 14:37:25 -0500

    Update client.py

--- client.py
+++ client.py
@@ -4,15 +4,15 @@ import json
 HOST = "18.117.252.35"
 PORT = 12345
 
-while True:
+while True:                                            #  allows multiple operations in one session
 
-    print("\nSimple Calculator")
+    print("\nSimple Calculator")                        # displays information to the user.
     print("Operations: add, sub, mul, div")
     print("Type exit to finish")
 
-    operation = input("Enter operation: ")
+    operation = input("Enter operation: ")                # gets the operation from the user, but it does not validate it.
 
-    if operation == "exit":
+    if operation == "exit":                               # It is used to close the program.
         print("Goodbye!")
         break
 
